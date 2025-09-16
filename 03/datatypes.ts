adbf26fe88333f2851af35e6c01472d02f3fde15
Devendra <devendra94kumawat@gmail.com>
2025-09-16 12:48:22 +0530

    DataTypes in TypeScript.

---
+++ 03/datatypes.ts
@@ -0,0 +1,43 @@
+// var num: number = 10;
+// var newNum: number = 20.6;
+// console.log(num);
+// console.log(newNum);
+
+// var newUserName: string = "Devendra Kumawat";
+// console.log(newUserName);
+
+// var isloggedIn: boolean = true;
+// console.log(isloggedIn);
+
+// var newVal: null = null;
+// console.log(newVal);
+
+// DataTypes in TypeScript :
+// 1. Primitive Types :
+// 1.1 Number
+// 1.2 String
+// 1.3 Boolean
+// 1.4 null
+// 1.5 undefined
+// 1.6 Symbol
+
+// 2. Object Types :
+// 2.1 Array
+// 2.2 Tuple
+// 2.3 Object
+
+// 3. Special Types :
+// 3.1 any
+// 3.2 unknown
+// 3.3 void
+// 3.4 never
+
+// 4. Advanced Types :
+// 4.1 Union
+// 4.2 Intersection
+// 4.3 Type Alias
+// 4.4 Enim
+// 4.5 Literal Types
+
+// 5. Function Types :
+// 5.1 Function Declaration - defines the type of function
