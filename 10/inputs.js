439b239bb0bc5d1643263b7a454294b85e0e31bb
Devendra <devendra94kumawat@gmail.com>
2025-09-16 17:43:13 +0530

    Ts with HTML input fields.

---
+++ 10/inputs.js
@@ -0,0 +1,9 @@
+function getInfo() {
+    var nameInput = document.getElementById("username");
+    var name = nameInput.value;
+    var emailInput = document.getElementById("email");
+    var email = emailInput.value;
+    var ageInput = document.getElementById("age");
+    var age = ageInput.value;
+    console.log(name, email, age);
+}
