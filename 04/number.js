543b1b14fb9f13b57ca622c557f41a72a2cf8289
Devendra <devendra94kumawat@gmail.com>
2025-09-16 15:14:47 +0530

    Number DataType in Ts

---
+++ 04/number.js
@@ -0,0 +1,15 @@
+var num = 10;
+var num2 = 20;
+var total = num + num2;
+console.log(total);
+var oct = 32769;
+var hexa = 0x00001;
+var binary = 1;
+console.log(oct);
+console.log(hexa);
+console.log(binary);
+var item = 100;
+var item2 = "50";
+// var item2Con=Number(item2)
+var item2Con = +item2;
+console.log(item + item2);
---
+++ 04/number.ts
@@ -0,0 +1,21 @@
+var num: number = 10;
+var num2: number = 20;
+
+var total: number = num + num2;
+console.log(total);
+
+var oct: number = 0o100001;
+var hexa: number = 0x00001;
+var binary: number = 0b00001;
+
+console.log(oct);
+console.log(hexa);
+console.log(binary);
+
+var item: number = 100;
+var item2 = "50";
+// var item2Con=Number(item2)
+var item2Con = +item2;
+console.log(item + item2);
+
+
