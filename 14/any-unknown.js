bc4fbbb22e25109d65f6f867b8b884be74f9231e
Devendra <devendra94kumawat@gmail.com>
2025-09-17 14:02:15 +0530

    Any-Unknown in Ts

---
+++ 14/any-unknown.js
@@ -0,0 +1,15 @@
+var value = 10;
+console.log(value);
+value = "Aryan";
+console.log(value);
+value = true;
+console.log(value);
+value = undefined;
+console.log(value);
+//
+var value2 = 10;
+console.log(value2);
+value2 = "Aryan";
+console.log(value2);
+value2 = true;
+console.log(value2);
---
+++ 14/any-unknown.ts
@@ -0,0 +1,22 @@
+let value: any = 10;
+console.log(value);
+
+value = "Aryan";
+console.log(value);
+
+value = true;
+console.log(value);
+
+value = undefined;
+console.log(value);
+
+//
+
+let value2: unknown = 10;
+console.log(value2);
+
+value2 = "Aryan";
+console.log(value2);
+
+value2 = true;
+console.log(value2);
