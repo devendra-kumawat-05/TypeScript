1330d19aa56c8a43a3d07066f57588dce0b0fda7
Devendra <devendra94kumawat@gmail.com>
2025-09-17 16:42:54 +0530

    Function in Ts

---
+++ 15/function.js
@@ -0,0 +1,11 @@
+function fruits() {
+    return "Apple";
+}
+console.log(fruits());
+function mark() {
+    return 10;
+}
+console.log(mark());
+function complex() {
+    return "Aryan";
+}
---
+++ 15/function.ts
@@ -0,0 +1,17 @@
+function fruits(): string {
+  return "Apple";
+}
+
+console.log(fruits());
+
+function mark(): number {
+  return 10;
+}
+
+console.log(mark());
+
+
+function complex() : string | number {
+    return "Aryan";
+
+}
