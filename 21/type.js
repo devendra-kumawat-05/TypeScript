9270a34339e18e54d2b66c719667bece7ef8e5a5
Devendra <devendra94kumawat@gmail.com>
2025-09-18 13:46:52 +0530

    Types in Ts

---
+++ 21/type.js
@@ -0,0 +1,10 @@
+var empData = {
+    name: "Devendra",
+    age: 25,
+};
+var empData2 = {
+    name: "Devendra",
+    age: 25,
+};
+console.log(empData);
+console.log(empData2);
---
+++ 21/type.ts
@@ -0,0 +1,16 @@
+type employee = {
+  name: string;
+  age: number;
+};
+
+let empData: employee = {
+  name: "Devendra",
+  age: 25,
+};
+let empData2: employee = {
+  name: "Devendra",
+  age: 25,
+};
+
+console.log(empData);
+console.log(empData2);
