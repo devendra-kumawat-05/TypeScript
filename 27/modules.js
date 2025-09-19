4ebb0653d5a20f9cfe36b6dca768c9fbbb11f604
Devendra <devendra94kumawat@gmail.com>
2025-09-19 12:17:01 +0530

    Modules in Ts

---
+++ 27/modules.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var userInfo = {
+    name: "Devendra",
+    age: 25,
+    email: "devendra@.com",
+    password: "123456",
+};
+console.log(userInfo);
---
+++ 27/modules.ts
@@ -0,0 +1,10 @@
+import type userIn from "./type";
+
+let userInfo: userIn = {
+  name: "Devendra",
+  age: 25,
+  email: "devendra@.com",
+  password: "123456",
+};
+
+console.log(userInfo);
