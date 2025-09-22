af027721b7d103204bd70f83b3c052cc1977e11a
Devendra <devendra94kumawat@gmail.com>
2025-09-22 12:03:56 +0530

    Index Signature in Ts

---
+++ 34/indexSignature.js
@@ -0,0 +1,10 @@
+// type userDataType12 = {
+//   [key: string]: number | string;
+// };
+var userData12 = {
+    mobile: 9999,
+    id: 10,
+    marks: 90,
+    age: 40,
+    semester: 3,
+};
---
+++ 34/indexSignature.ts
@@ -0,0 +1,27 @@
+// type userDataType12 = {
+//   [key: string]: number | string;
+// };
+
+// let userData12 = {
+//   mobile: 9999,
+//   id: 10,
+//   marks: 90,
+//   age: 40,
+//   semester: 3,
+// };
+
+type userDataType12 = {
+  name: string;
+  id: number;
+  mobile: number;
+  //   readonly [key: string]: number | string;
+  [key: string]: number | string;
+};
+
+let userData12 = {
+  mobile: 9999,
+  id: 10,
+  marks: 90,
+  age: 40,
+  semester: 3,
+};
